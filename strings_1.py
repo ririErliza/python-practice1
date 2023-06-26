3ef43a4d91b7c645bc6e7ef1ea2ba2703019f38d
Erliza Erbaryanti <95483807+ririErliza@users.noreply.github.com>
2023-06-26 11:49:28 +0200

    Update strings_1.py

--- strings_1.py
+++ strings_1.py
@@ -1,1 +1,19 @@
-'hello'
+print('hello')
+
+print("hello")
+
+print("he's a mad man")
+
+print('he\'s a mad man')
+
+greet = 'hello'
+
+print(greet)
+
+print(greet[0])
+
+print(greet[1])
+
+print(greet[-1])
+
+print(greet[-5])
