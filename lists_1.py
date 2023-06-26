b1139f16c3dbf88183875a58bc607b4c93fe1ad1
Erliza Erbaryanti <95483807+ririErliza@users.noreply.github.com>
2023-06-26 12:06:35 +0200

    update

---
+++ lists_1.py
@@ -0,0 +1,25 @@
+str = 'hello how are you?'
+
+# use split to make a list from a string variable
+
+print(str.split(' '))
+# ['hello', 'how', 'are', 'you?']
+
+
+
+fib1 = [1,1,2,3,5,8,13]
+print(fib1)
+
+print(fib1[0])
+
+print(fib1[5])
+
+print(fib1[-1])
+
+print(fib1[-3])
+
+print(fib1[0:4])
+
+
+# Concatenate Lists
+
--- strings_1.py
+++ strings_1.py
@@ -8,12 +8,39 @@ print('he\'s a mad man')
 
 greet = 'hello'
 
-print(greet)
+print(greet) #hello
 
-print(greet[0])
+print(greet[0]) # h
 
-print(greet[1])
+print(greet[1]) # e
 
-print(greet[-1])
+print(greet[-1]) # o -- from the back
 
-print(greet[-5])
+print(greet[-5]) # h
+
+#slice
+print(greet[0:3]) # hel - from position 0 to position 3
+
+print(greet[-1:2]) # return nothing
+
+#concatinating
+str1='yeay'
+str2='dude'
+print(str1+str2) # yeaydude
+
+print(str1+' '+str2) # yeay dude
+
+#----------
+
+
+
+print(str1*3) # yeayyeayyeay
+
+#----------
+
+# STRING METHODS
+
+print(str1.upper()) # YEAY
+
+cheeses = 'brie, cheddar, edam'
+print(cheeses.split(',')) # ['brie', ' cheddar', ' edam']
