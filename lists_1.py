c8e4251911889ed01b9cd5bf6d0b76ce3f5a7b2e
Erliza Erbaryanti <95483807+ririErliza@users.noreply.github.com>
2023-06-26 12:13:37 +0200

    Update lists_1.py

--- lists_1.py
+++ lists_1.py
@@ -8,18 +8,34 @@ print(str.split(' '))
 
 
 fib1 = [1,1,2,3,5,8,13]
-print(fib1)
+print(fib1)             # [1, 1, 2, 3, 5, 8, 13]
 
-print(fib1[0])
+print(fib1[0])          # 1
 
-print(fib1[5])
+print(fib1[5])          # 8
 
-print(fib1[-1])
+print(fib1[-1])         # 13
 
-print(fib1[-3])
+print(fib1[-3])         # 5
 
-print(fib1[0:4])
+print(fib1[0:4])        # [1, 1, 2, 3]
 
 
 # Concatenate Lists
+
+fib2 = [21, 34, 55]
+
+print(fib1 + fib2)      # [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+
+
+
+# Change the values of the list
+fib1[0] = 9
+print(fib1) # [9, 1, 2, 3, 5, 8, 13]
+
+
+# append value to list
+fib2.append(89)
+print(fib2)
+
 
