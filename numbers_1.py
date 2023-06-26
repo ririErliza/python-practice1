49fe934effd0266270c2619167efc4be576fdb4e
Erliza Erbaryanti <95483807+ririErliza@users.noreply.github.com>
2023-06-26 11:41:59 +0200

    Create numbers_1.py

---
+++ numbers_1.py
@@ -0,0 +1,30 @@
+print('1. ', 5+5)
+
+print('2. ', 5-5)
+
+print('3. ', 5*5)
+
+print('4. ', 5/5)
+
+print('5. ', 5//5)
+
+print('6. ', 5%2)
+
+print('7. ', (5+5)*3)
+
+age=25
+print('8. ', age)
+
+print('9. ', age+5)
+
+age=age+5
+print('10. ', age)
+
+age+=5
+print('11. ', age)
+
+age-=5
+print('12. ', age)
+
+age/=2
+print('13. ', age)
