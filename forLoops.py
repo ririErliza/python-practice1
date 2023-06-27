6ff06238580018b8a7753ec4617078a83071599b
Erliza Erbaryanti <95483807+ririErliza@users.noreply.github.com>
2023-06-27 10:09:07 +0200

    update

--- forLoops.py
+++ forLoops.py
@@ -2,8 +2,8 @@ ninjas = ['ryu', 'chrystal', 'yoshi', 'ken']
 
 # use for loop to access individual ninja from the list ninjas
 
-for ninja in ninjas:
-    print(ninja)
+# for ninja in ninjas:
+#     print(ninja)
 
 # ryu
 # chrystal
@@ -23,15 +23,56 @@ for ninja in ninjas:
 
 fruits = ['banana', 'apple', 'oranges', 'grapes']
 
-for x in fruits[1:3]:
-    print(x)
+# for x in fruits[1:3]:
+#     print(x)
 
 # apple
 # oranges
 
-for fruit in fruits:
-    if fruit == 'apple':
-        print(f'{fruit} is sweet!')
+# for fruit in fruits:
+#     if fruit == 'apple':
+#         print(f'{fruit} is sweet!')
 
 # apple is sweet!
+
+
+# for fruit in fruits:
+#     if fruit == 'kiwi':
+#         print(f'{fruit} is green!')
+#     else:
+#         print(fruit)
+
+# banana
+# apple
+# oranges
+# grapes
+
+
+# (because the is no kiwi on the list)
+
+# for fruit in fruits:
+#     if fruit == 'oranges':
+#         print(f'{fruit} is sweet!')
+#     else:
+#         print(fruit)
+
+# banana
+# apple
+# oranges is sweet!
+# grapes
+
+for fruit in fruits:
+    if fruit == 'oranges':
+        print(f'{fruit} is sweet!')
+        break
+    else:
+        print(fruit)
+
+# banana
+# apple
+# oranges is sweet!
+
+# (after looping through and found oranges, it prints the fruit and 
+# other fruits on the list preceded oranges
+# but because of 'break' operation, it stopped looping after oranges)
 
