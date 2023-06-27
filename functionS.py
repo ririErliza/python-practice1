1b5d7d9f8c9e8758042e514f3a995b949fa738f0
Erliza Erbaryanti <95483807+ririErliza@users.noreply.github.com>
2023-06-27 11:27:07 +0200

    Update functionS.py

--- functionS.py
+++ functionS.py
@@ -5,6 +5,8 @@ def greet():
 
 greet() # Hello World
 
+print('---------------------------------')
+
 
 def hello(name,time):
     print(f'Good {time} {name}, hope you are well')
@@ -19,3 +21,17 @@ time=input('enter your time of day: ')
 hello(name,time)
 # Good afternoon Erliz, hope you are well
 
+
+print('---------------------------------')
+
+def hello(name='Ryu',time='morning'):
+    print(f'Good {time} {name}, hope you are well')
+
+hello()
+# Good morning Ryu, hope you are well
+
+hello('Shaun', 'Afternoon') #this will overwrite the defined parameter above
+# Good Afternoon Shaun, hope you are well
+
+hello(name='Shaun')
+# Good morning Shaun, hope you are well
