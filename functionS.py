e8c721dcff4699087f99d1d6ebf9c39722767f83
Erliza Erbaryanti <95483807+ririErliza@users.noreply.github.com>
2023-06-27 11:36:08 +0200

    Update functionS.py

--- functionS.py
+++ functionS.py
@@ -8,17 +8,17 @@ greet() # Hello World
 print('---------------------------------')
 
 
-def hello(name,time):
-    print(f'Good {time} {name}, hope you are well')
+# def hello(name,time):
+#     print(f'Good {time} {name}, hope you are well')
 
 
-hello('Shaun', 'morning')
-# Good morning Shaun, hope you are well
+# hello('Shaun', 'morning')
+# # Good morning Shaun, hope you are well
 
-name=input('enter your name: ')
-time=input('enter your time of day: ')
+# name=input('enter your name: ')
+# time=input('enter your time of day: ')
 
-hello(name,time)
+# hello(name,time)
 # Good afternoon Erliz, hope you are well
 
 
@@ -35,3 +35,32 @@ hello('Shaun', 'Afternoon') #this will overwrite the defined parameter above
 
 hello(name='Shaun')
 # Good morning Shaun, hope you are well
+
+print('---------------------------------')
+
+# calculating using function
+
+
+
+# def area(radius):
+#     print(3.142*radius*radius)
+
+# radius=int(input('enter radius: '))
+
+# area(radius)
+
+print('---------------------------------')
+
+def area(radius):
+    return 3.142*radius*radius
+
+def vol(area,length):
+    print('The volume is:', area*length)
+
+radius=int(input('enter radius: '))
+length=int(input('enter a length: '))
+
+area_calc=area(radius)
+vol(area_calc,length)
+
+
