20d21501d261231d9b27c4110e2b2b226ec1c389
Erliza Erbaryanti <95483807+ririErliza@users.noreply.github.com>
2023-06-27 10:27:06 +0200

    Update whileLoops.py

--- whileLoops.py
+++ whileLoops.py
@@ -34,8 +34,34 @@ num = 0
 # 20
 
 # break it at certain condition
+# while num < age:
+#     if num%2 ==0:
+#         print(num)
+#     if num > 10:
+#         break
+#     num+=1
+
+# 0
+# 2
+# 4
+# 6
+# 8
+# 10
+
+# continue (we dont want to print the 0)
 while num < age:
+    if num == 0:
+        num +=1 #this is a must otherwise we will get an infinite loop
+        continue
     if num%2 ==0:
         print(num)
     if num > 10:
         break
+    num+=1
+
+# 2
+# 4
+# 6
+# 8
+# 10
+
