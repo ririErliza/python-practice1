451b9ff10f4ebc98c7fd6c6ee892014fcd082016
Erliza Erbaryanti <95483807+ririErliza@users.noreply.github.com>
2023-06-27 10:33:46 +0200

    update

---
+++ ranges_1.py
@@ -0,0 +1,27 @@
+items=[0,1,2,3,4,5]
+print(items)
+# [0, 1, 2, 3, 4, 5]
+
+
+
+# for n in range(6):
+#     print(n)
+
+# 0
+# 1
+# 2
+# 3
+# 4
+# 5
+
+for n in range(3,10):
+    print(n)
+
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9
+
