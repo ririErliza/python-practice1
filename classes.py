eddbf959eee367bcfefc0b85a60a94edcbcfa442
Erliza Erbaryanti <95483807+ririErliza@users.noreply.github.com>
2023-06-29 08:13:09 +0200

    Create classes.py

---
+++ classes.py
@@ -0,0 +1,22 @@
+# how to find out the type of a variable
+
+name = 'gisel'
+age = 33
+nums = [1,2,11,22,3,4,8]
+
+# just use type(theVariableName)
+
+print(type(name)) #<class 'str'>
+print(type(age)) #<class 'int'>
+print(type(nums)) #<class 'list'>
+
+#----------------------------------------------
+
+# Python Classes/Objects
+# Python is an object oriented programming 
+# language.
+# Almost everything in Python is an object, 
+# with its PROPERTIES and METHODS.
+# A Class is like an object constructor, 
+# or a "blueprint" for creating objects.
+
