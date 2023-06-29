5ff4968f0ea6f089817394462ef91200ac8fe083
Erliza Erbaryanti <95483807+ririErliza@users.noreply.github.com>
2023-06-29 08:30:24 +0200

    Update classes.py

--- classes.py
+++ classes.py
@@ -20,3 +20,50 @@ print(type(nums)) #<class 'list'>
 # A Class is like an object constructor, 
 # or a "blueprint" for creating objects.
 
+#----------------------------------------------
+
+class Planet:
+
+    def __init__(self):
+        self.name = 'Mars'
+        self.radius = 20000
+        self.gravity = 5.5
+        self.system = 'Mars System'
+
+Mars = Planet()
+print(f'name is: {Mars.name}')
+print(f'radius is: {Mars.radius}')
+print(f'gravity is: {Mars.gravity}')
+print(f'system is: {Mars.system}')
+
+# name is: Mars
+# radius is: 20000
+# gravity is: 5.5
+# system is: Mars System
+
+class laptop:
+
+    def __init__(self):
+        self.name = 'Acer'
+        self.ram = 20000
+        self.battery = 5.5
+        self.system = 'windows'
+
+    def os(self):
+        return f'{self.name} has {self.system} as it\'s OS'
+    
+acer=laptop()
+
+print(acer.os())
+# Acer has windows as it's OS
+
+# we attached method and properties to this class
+
+# if we declare another variable and attach the same class
+# that varibale will get the same poperties
+
+noBrand = laptop()
+print(noBrand.os())
+# Acer has windows as it's OS
+
+
